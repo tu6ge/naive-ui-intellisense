6453ac733e90306090550782ce1ba66045150c40
tu6ge <tu6ge@qq.com>
2025-09-01 13:22:25 +0800

    feat: 初始化 lsp

--- src/extension.ts
+++ src/extension.ts
@@ -1,93 +1,45 @@
-import { ExtensionContext } from 'vscode'
-import * as vscode from 'vscode'
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
-import { ElementHoverProvier } from './hover-tips/element-hover-provider'
-import { ElementCompletionItemProvider } from './completion/element-completion-item-povider'
-import { SlotCompletionItems } from './completion/slot-completion-items'
-import { PascalCaseCompletionItem } from './completion/PascalCaseCompletionItem'
-import { PascalCaseSlotCompletionItems } from './completion/PascalCaseSlotCompletionItems'
+let client: LanguageClient;
 
-export function activate(context: ExtensionContext): void {
-  console.log('extension "naive-ui" is now active!')
+export function activate(context: vscode.ExtensionContext) {
+  // 服务端可执行文件路径（Node 或 Rust 编译产物）
+  let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+  // 如果是 Rust，可以写成 ./target/debug/my-lsp-server
 
-  // 注册 completion 提示
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider(
-      [
-        {
-          language: 'vue',
-          scheme: 'file'
-        }
-      ],
-      new ElementCompletionItemProvider(),
-      '',
-      ' ',
-      ':',
-      '<',
-      '"',
-      "'",
-      '/',
-      '@',
-      '(',
-      '-'
-    )
-  )
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider(
-      [
-        {
-          language: 'vue',
-          scheme: 'file'
-        }
-      ],
-      new SlotCompletionItems(),
-      '#'
-    )
-  )
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider(
-      [
-        {
-          language: 'vue',
-          scheme: 'file'
-        }
-      ],
-      new PascalCaseCompletionItem(),
-      'N',
-      ' '
-    )
-  )
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider(
-      [
-        {
-          language: 'vue',
-          scheme: 'file'
-        }
-      ],
-      new PascalCaseSlotCompletionItems(),
-      '#'
-    )
-  )
+  let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-  // 注册 hover 提示
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      [
-        {
-          language: 'vue',
-          scheme: 'file'
-        }
-      ],
-      new ElementHoverProvier()
-    )
-  )
+  let serverOptions: ServerOptions = {
+    run: { 
+      module: serverModule, 
+      transport: TransportKind.ipc 
+    },
+    debug: { 
+      module: serverModule, 
+      transport: TransportKind.ipc,
+      options: debugOptions
+    }
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'vue' }], // 绑定语言
+  };
+
+  client = new LanguageClient(
+    'NaiveClient',
+    'My Naive Client',
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
-
-export default {
-  activate,
-  deactivate
+export function deactivate(): Promise<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
