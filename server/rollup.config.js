ac71023984fe3b69d57deac0ca41c172797f8b21
tu6ge <tu6ge@qq.com>
2026-02-06 13:23:31 +0800

    fix: 解决调试无法运行的问题
    部分文件执行了代码格式化

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,9 +1,9 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser'
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production'
 
 export default [
   {
@@ -14,25 +14,14 @@ export default [
       sourcemap: !production,
       exports: 'auto'
     },
-    external: [
-      'vscode',
-      'vscode-languageserver',
-      'vscode-languageserver-textdocument',
-      'https',
-      'fs',
-      'path',
-      'util',
-      'crypto',
-      'stream',
-      'events'
-    ],
+    external: ['vscode', 'vscode-languageserver', 'vscode-languageserver-textdocument', 'https', 'fs', 'path', 'util', 'crypto', 'stream', 'events'],
     plugins: [
       resolve({
         preferBuiltins: true
       }),
       commonjs(),
       json(),
-      production && terser(),
+      production && terser()
     ]
   }
-];
+]
--- server/scripts/prebuild.js
+++ server/scripts/prebuild.js
@@ -1,17 +1,17 @@
 // 在构建前下载所有组件文档并保存为 JSON
-const { NaiveUIMetadataExtractor } = require('../out/metadata');
+const { NaiveUIMetadataExtractor } = require('../out/metadata')
 const fs = require('fs')
 
 async function prebuild() {
-  console.log('正在删除久版缓存文件');
-  fs.rmSync("./.cache/", { recursive: true, force: true });
+  console.log('正在删除久版缓存文件')
+  fs.rmSync('./cache/', { recursive: true, force: true })
 
-  console.log('📥 预下载 Naive UI 组件文档...');
-  
-  const metadataExtractor = new NaiveUIMetadataExtractor(true);
-  
+  console.log('📥 预下载 Naive UI 组件文档...')
+
+  const metadataExtractor = new NaiveUIMetadataExtractor(true)
+
   // 保存到 server/data/components.json
-  metadataExtractor.initialize();
+  metadataExtractor.initialize()
 }
 
-prebuild();
+prebuild()
